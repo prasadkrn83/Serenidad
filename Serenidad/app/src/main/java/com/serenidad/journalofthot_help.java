e552a0a4985cbbd39ec4190f499896ccc75a9f83
Omkar Sahasrabudhe <omkars99@gmail.com>
2019-04-27 20:53:00 -0500

    Added Journal of Thoughts Module , Screen 1 is dummy , Waiting for Prasads input for Screen 1 (Emotion meter)

---
+++ Serenidad/app/src/main/java/com/serenidad/journalofthot_help.java
@@ -0,0 +1,14 @@
+package com.serenidad;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class journalofthot_help extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_journalofthot_help);
+    }
+}
+
