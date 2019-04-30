291e48e91b782a9f73c26b0f903aef90aa846324
Omkar Sahasrabudhe <omkars99@gmail.com>
2019-04-30 12:08:51 -0500

    Revert "Spanish Content fix"
    This reverts commit 3b6d5db46b05269f6e1438ef2347d07287f433cd.

--- Serenidad/app/src/main/java/com/serenidad/journalofthot_help.java
+++ Serenidad/app/src/main/java/com/serenidad/journalofthot_help.java
@@ -1,9 +1,7 @@
 package com.serenidad;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
 public class journalofthot_help extends AppCompatActivity {
 
@@ -12,16 +10,5 @@ public class journalofthot_help extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_journalofthot_help);
     }
-
-
-    public void BackHome(View V)
-    {
-        Intent intent = new Intent(this,Home.class);
-        startActivity(intent);
-
-
-    }
-
-
 }
 
