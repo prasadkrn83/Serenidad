4660b5988f8b03043c52d8c7a8aa85de06a68f17
Shekhar Chaudhary <shekhar_chaudhary@outlook.com>
2019-04-30 13:37:07 -0500

    contact us back button

--- Serenidad/app/src/main/java/com/serenidad/ContactUs.java
+++ Serenidad/app/src/main/java/com/serenidad/ContactUs.java
@@ -2,12 +2,22 @@ package com.serenidad;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class ContactUs extends AppCompatActivity {
-
+    LinearLayout linearLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_us);
+        linearLayout = findViewById(R.id.button);
+
+        linearLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
     }
 }
